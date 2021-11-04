7b0d3a7322de8ecea376326447dc712f0e900a71
JestiferM <jestifermartinez01@gmail.com>
2021-11-03 21:00:50 -0400

    working on challenges

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import axios from 'axios';
 
 import "./App.css";
 
-useEffect(()=>{
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  .then(res =>{
-    console.log(res.data);
-  })
-  .catch(err => {
-    console.error(err);
-  })
-},[])
 
 function App() {
+
+   const [data, setData] = useState({});
+
+   useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then((res) =>{
+      console.log(res.data)
+      setData(res.data)
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  },[])
+  
   return (
     <div className="App">
-      <h1> NASA as I develope it </h1>
+      <h1> NASA Photos </h1>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
       <div className='Img-container'>
-
+        <img src={data.url} alt={data.title}></img>
       </div>
     </div>
   );
