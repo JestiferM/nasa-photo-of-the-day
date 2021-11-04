4b852ed75fba2a1b78959af05ae7e2f1f50735e6
JestiferM <jestifermartinez01@gmail.com>
2021-11-03 20:00:52 -0400

    jessy-martinez

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
+
 import "./App.css";
+
+useEffect(()=>{
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(res =>{
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+},[])
 
 function App() {
   return (
     <div className="App">
+      <h1> NASA as I develope it </h1>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <div className='Img-container'>
+
+      </div>
     </div>
   );
 }
